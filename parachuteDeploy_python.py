fb2491f27965f8ba3da1b09bc719a0fb6a268afd
pbleved <75136017+pbleved@users.noreply.github.com>
2020-11-27 13:06:24 -0500

    Update parachuteDeploy_python.py
    Final changes to python onboarding task

--- parachuteDeploy_python.py
+++ parachuteDeploy_python.py
@@ -1,61 +1,67 @@
 import random
 from time import sleep
 
-iterationFactor: int = 1
-simpleCounter: int = 0
-noisyCounter: int = 0
+iterationFactor = 1
+counter = 0
+upperLimit = round(random.uniform(300, 360), 0)
 pdTrigger = False
 
 
+# this function outputs a single value iterating over time simulating altitude outputs
 def simpleSensor_Function():
     global iterationFactor
-    global simpleCounter
+    global counter
+    global upperLimit
+
     dataValue = round(random.uniform(0, 9), 2) + iterationFactor
 
-    if simpleCounter < 150:
-        iterationFactor += 70
+    if counter < upperLimit:
+        iterationFactor += 20
     else:
-        iterationFactor -= 70
+        iterationFactor -= 20
 
-    simpleCounter += 1
-    
-    if simpleCounter >= 302:
+    counter += 1
+
+    if counter >= upperLimit * 2:
         raise Exception("It appears we've turned this rocket into a lawn dart! MISSION FAILED")
-    
+
     sleep(0.1)
     return dataValue
 
 
+# this function outputs an array of 3 values iterating over time simulating altitude outputs with noise
 def noisySensor_Function():
-    global noisyCounter
+    global counter
     global iterationFactor
+    global upperLimit
     dataSet = []
 
     for i in range(3):
         dataSet.append(round(random.uniform(4, 7), 2) + iterationFactor)
-        if noisyCounter <= 150:
-            iterationFactor += 10
+        if counter <= upperLimit:
+            iterationFactor += 20
         else:
-            iterationFactor -= 10
+            iterationFactor -= 20
 
-    noisyCounter += 1
-    
-    if noisyCounter >= 302:
+    counter += 1
+
+    if counter >= upperLimit * 2:
         raise Exception("It appears we've turned this rocket into a lawn dart! MISSION FAILED")
-        
+    
     sleep(0.1)
     return dataSet
 
 
 def user_Function():
-    # display max altitude, height at which parachute is deployed, flight time (opt.)
+    # display max altitude, height at which parachute is deployed, flight time.
     while not pdTrigger:
         # Write code here
-        
-        print(simpleSensor_Function(), "   Counter: ", simpleCounter)
-        print(noisySensor_Function(), "    Counter: ", noisyCounter, "\n")
-        
+        # Implement either the simple sensor or noisy sensor function when completing the program.
 
+        # Example outputs:
+        print(simpleSensor_Function(), "   Counter: ", counter)
+        print(noisySensor_Function(), "    Counter: ", counter, "\n")
+    
 
 user_Function()
 
